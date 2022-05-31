1cecc8e10f888a17b4ca4d7aff70ec7d1ea9cbef
Ben Reiner <benjamin.t.reiner@gmail.com>
2022-05-31 12:25:29 -0400

    Style search bar

--- src/components/CountrySearch.js
+++ src/components/CountrySearch.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import "../styles/CountrySearch.css";
 
 class CountrySearch extends Component {
   constructor(props) {
@@ -9,6 +13,10 @@ class CountrySearch extends Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
+  componentDidMount() {
+    library.add(faMagnifyingGlass);
+  }
+
   handleChange(e) {
     this.setState({ value: e.target.value }, () =>
       this.props.filter(this.state.value)
@@ -17,14 +25,19 @@ class CountrySearch extends Component {
 
   render() {
     return (
-      <input
-        className="CountrySearch"
-        placeholder="Search for a country"
-        id="search"
-        name="search"
-        value={this.state.value}
-        onChange={this.handleChange}
-      />
+      <div
+        className={`CountrySearch ${this.props.darkMode ? "dark" : "light"}`}
+      >
+        <FontAwesomeIcon icon="magnifying-glass" />
+        <input
+          className="CountrySearch-field"
+          placeholder="Search for a country..."
+          id="search"
+          name="search"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </div>
     );
   }
 }
